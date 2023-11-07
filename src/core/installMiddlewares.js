670dca410726f00de11ac606947f340d33cb585d
Brecht Vandekerckhove <brecht.vandekerckhove@student.hogent.be>
2023-11-07 23:03:53 +0000

    request logging

--- src/core/installMiddlewares.js
+++ src/core/installMiddlewares.js
@@ -1,9 +1,11 @@
-const config = require('config');
-const bodyParser = require('koa-bodyparser');
-const koaCors = require('@koa/cors');
+const config = require("config");
+const bodyParser = require("koa-bodyparser");
+const koaCors = require("@koa/cors");
+const emoji = require("node-emoji");
+const { getLogger } = require("./logging");
 
-const CORS_ORIGINS = config.get('cors.origins');
-const CORS_MAX_AGE = config.get('cors.maxAge');
+const CORS_ORIGINS = config.get("cors.origins");
+const CORS_MAX_AGE = config.get("cors.maxAge");
 
 /**
  * Install all required middlewares in the given app.
@@ -11,7 +13,7 @@ const CORS_MAX_AGE = config.get('cors.maxAge');
  * @param {koa.Application} app - The Koa application.
  */
 module.exports = function installMiddleware(app) {
-   app.use(
+  app.use(
     koaCors({
       origin: (ctx) => {
         if (CORS_ORIGINS.indexOf(ctx.request.header.origin) !== -1) {
@@ -20,11 +22,38 @@ module.exports = function installMiddleware(app) {
         // Not a valid domain at this point, let's return the first valid as we should return a string
         return CORS_ORIGINS[0];
       },
-      allowHeaders: ['Accept', 'Content-Type', 'Authorization'],
+      allowHeaders: ["Accept", "Content-Type", "Authorization"],
       maxAge: CORS_MAX_AGE,
     })
   );
+  app.use(async (ctx, next) => {
+    getLogger().info(`${emoji.get("fast_forward")} ${ctx.method} ${ctx.url}`); 
 
+    const getStatusEmoji = () => {
+      if (ctx.status >= 500) return emoji.get("skull");
+      if (ctx.status >= 400) return emoji.get("x");
+      if (ctx.status >= 300) return emoji.get("rocket");
+      if (ctx.status >= 200) return emoji.get("white_check_mark");
+      return emoji.get("rewind");
+    };
+
+    try {
+      await next();
+
+      getLogger().info(
+        `${getStatusEmoji()} ${ctx.method} ${ctx.status} ${ctx.url}`
+      );
+    } catch (error) {
+      getLogger().error(
+        `${emoji.get("x")} ${ctx.method} ${ctx.status} ${ctx.url}`,
+        {
+          error,
+        }
+      );
+
+      throw error;
+    }
+  });
   app.use(bodyParser());
 
   // Handle 404 not found with uniform response
@@ -34,7 +63,7 @@ module.exports = function installMiddleware(app) {
     if (ctx.status === 404) {
       ctx.status = 404;
       ctx.body = {
-        code: 'NOT_FOUND',
+        code: "NOT_FOUND",
         message: `Unknown resource: ${ctx.url}`,
       };
     }
